019183edbd92877a0071684e6f7a0cc188cb9451
hurshidbe <egamberdiyevx8@gmail.com>
2025-09-27 20:57:17 +0500

    react app url fixed

--- src/services/api.ts
+++ src/services/api.ts
@@ -19,7 +19,7 @@ class ApiService {
   private baseURL: string;
 
   constructor() {
-    this.baseURL = process.env.REACT_APP_API_URL || "http://hr_bck:5000";
+    this.baseURL = process.env.REACT_APP_API_URL || "https://hr-bck.hurshidbe.uz";
     http: console.log("API Service initialized with baseURL:", this.baseURL);
 
     this.api = axios.create({
